5a130180e9c949b3f1f074518f22e12dec038065
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-01-31 16:47:37 +0000

    Fix down from migrations

--- database/migrations/2021_01_27_170454_add_google_id_column.php
+++ database/migrations/2021_01_27_170454_add_google_id_column.php
@@ -25,6 +25,8 @@ class AddGoogleIdColumn extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('google_id');
+        });
     }
 }
