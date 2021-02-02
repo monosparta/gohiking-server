b3a413e2aa366fed0ab66103f2431211ac658028
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-02-02 04:30:34 +0000

    Tweak coding style

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -10,36 +10,38 @@ use Illuminate\Support\Facades\Auth;
 
 class FacebookController extends Controller
 {
-    public function redirectToFacebook(){
+    public function redirectToFacebook()
+    {
         return Socialite::driver('facebook')->redirect();
     }
 
-    public function handleFacebookCallback(){
-        try{
+    public function handleFacebookCallback()
+    {
+        try {
             $user = Socialite::driver('facebook')->user();
             $findUser = User::where('email', $user->email)->first();
-            
-            if($findUser->facebook_id){ // 帳號已經綁定FB
+
+            if ($findUser->facebook_id) { // 帳號已經綁定FB
                 Auth::login($findUser);
 
-                $token = $findUser->user()->createToken('LaravelAuthApp')->accessToken;                
+                $token = $findUser->user()->createToken('LaravelAuthApp')->accessToken;
                 return response()->json(['token' => $token], 200);
                 // return redirect('dashboard')->with('message', 'Logged in!'); // API不需要前端頁面
             }
-            if($findUser && $findUser->facebook_id == NULL ){ // 帳號已經註冊，但未綁定FB
+            if ($findUser && $findUser->facebook_id == NULL) { // 帳號已經註冊，但未綁定FB
                 $updateUser = User::where('email', $user->email)->update([
-                    'facebook_id' => $user->id 
-                ]); 
-                if (!$updateUser){
+                    'facebook_id' => $user->id
+                ]);
+                if (!$updateUser) {
                     return response()->json(['message' => 'You can not loggin'], 401);
                     // return redirect('welcome')->with('message', 'You can not loggin');
                 }
-                $token = $findUser->user()->createToken('LaravelAuthApp')->accessToken;     
+                $token = $findUser->user()->createToken('LaravelAuthApp')->accessToken;
                 return response()->json(['token' => $token], 200);
                 // return redirect('dashboard')->with('message', 'Logged in!');
             }
-            
-            if(!$findUser){ // 帳號尚未註冊
+
+            if (!$findUser) { // 帳號尚未註冊
                 $newUser = User::create([
                     'name' => $user->name,
                     'email' => $user->email,
@@ -48,11 +50,11 @@ class FacebookController extends Controller
                 ]);
                 $newUser->save();
                 Auth::login($newUser);
-                $token = $newUser->createToken('LaravelAuthApp')->accessToken; 
+                $token = $newUser->createToken('LaravelAuthApp')->accessToken;
                 return response()->json(['token' => $token], 200);
                 // return redirect('dashboard')->with('message', 'Logged in!');
-            }              
-        }catch(Exception $e){
+            }
+        } catch (Exception $e) {
             dd($e->getMessage());
         }
     }
