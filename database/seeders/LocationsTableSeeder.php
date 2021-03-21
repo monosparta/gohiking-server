a2a868fc6c411e066833b6be65d85b98bb041e29
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-03-21 17:21:33 +0800

    Add autoIncrementTweak for different environment

--- database/seeders/LocationsTableSeeder.php
+++ database/seeders/LocationsTableSeeder.php
@@ -15,59 +15,45 @@ class LocationsTableSeeder extends Seeder
      */
     public function run()
     {
-        function cleardbAutoIncrement($id) {
+        function autoIncrementTweak($id) {
             $range = 4; // 根據ClearDB設定
             return $id*10-10+$range;
+
+            // return $id; // 本機設定
         }
 
         $datas = [
             [
                 'name' => '北投區',
-                'county_id' =>
-                // 1,
-                cleardbAutoIncrement(1),
+                'county_id' => autoIncrementTweak(1),
             ],
             [
                 'name' => '復興區',
-                'county_id' =>
-                // 3,
-                cleardbAutoIncrement(3),
+                'county_id' => autoIncrementTweak(3),
             ],
             [
                 'name' => '復興鄉',
-                'county_id' =>
-                // 3,
-                cleardbAutoIncrement(3),
+                'county_id' => autoIncrementTweak(3),
             ],
             [
                 'name' => '北屯區',
-                'county_id' =>
-                // 7,
-                cleardbAutoIncrement(7),
+                'county_id' => autoIncrementTweak(7),
             ],
             [
                 'name' => '南屯區',
-                'county_id' =>
-                // 7,
-                cleardbAutoIncrement(7),
+                'county_id' => autoIncrementTweak(7),
             ],
             [
                 'name' => '新烏日',
-                'county_id' =>
-                // 7,
-                cleardbAutoIncrement(7),
+                'county_id' => autoIncrementTweak(7),
             ],
             [
                 'name' => '沙鹿區',
-                'county_id' =>
-                // 7,
-                cleardbAutoIncrement(7),
+                'county_id' => autoIncrementTweak(7),
             ],
             [
                 'name' => '谷關區',
-                'county_id' =>
-                // 7,
-                cleardbAutoIncrement(7),
+                'county_id' => autoIncrementTweak(7),
             ],
         ];
         foreach ($datas as $data) {
--- database/seeders/TrailsTableSeeder.php
+++ database/seeders/TrailsTableSeeder.php
@@ -15,72 +15,81 @@ class TrailsTableSeeder extends Seeder
      */
     public function run()
     {
+        function autoIncrementTweak($id)
+        {
+            $range = 4; // 根據ClearDB設定
+            return $id * 10 - 10 + $range;
+
+            // return $id; // 本機設定
+        }
+
+
         $datas = [
             [
                 'title' => '東眼山自導式步道',
-                'location_id' => 3,
+                'location_id' =>  autoIncrementTweak(3),
                 'distance' => 4000,
                 'coverImage' => 'https://i.imgur.com/c0NFa4Q.jpg',
                 'difficulty' => 3,
                 'evaluation' => 3,
                 'altitude' => 2000,
-                'article_id' => 1,
-                'classification_id' => 3,
+                'article_id' =>  autoIncrementTweak(1),
+                'classification_id' =>  autoIncrementTweak(3),
             ],
             [
                 'title' => '巴陵古道',
-                'location_id' => 2,
+                'location_id' =>  autoIncrementTweak(2),
                 'distance' => 1500,
                 'coverImage' => 'https://i.imgur.com/vLdAblX.jpg',
                 'difficulty' => 1,
                 'evaluation' => 4,
                 'altitude' => 3000,
-                'article_id' => 2,
-                'classification_id' => 1,
+                'article_id' =>  autoIncrementTweak(2),
+                'classification_id' =>  autoIncrementTweak(1),
             ],
             [
                 'title' => '嘎拉賀野溪溫泉步道',
-                'location_id' => 3,
+                'location_id' =>  autoIncrementTweak(3),
                 'distance' => 1600,
                 'coverImage' => 'https://i.imgur.com/QesUZAo.jpg',
                 'difficulty' => 2,
                 'evaluation' => 3,
                 'altitude' => 1000,
-                'article_id' => 3,
-                'classification_id' => 2,
+                'article_id' =>  autoIncrementTweak(3),
+                'classification_id' =>  autoIncrementTweak(2),
             ],
             [
                 'title' => '消波塊步道',
-                'location_id' => 7,
+                'location_id' =>  autoIncrementTweak(7),
                 'distance' => 2000,
                 'coverImage' => 'https://i.imgur.com/ccmgvp4.jpg',
                 'difficulty' => 5,
                 'evaluation' => 5,
                 'altitude' => 0,
-                'article_id' => 1,
-                'classification_id' => 3,
+                'article_id' =>  autoIncrementTweak(1),
+                'classification_id' =>  autoIncrementTweak(3),
             ],
             [
                 'title' => '新鮮空氣步道',
-                'location_id' => 8,
+                'location_id' =>  autoIncrementTweak(8),
                 'distance' => 5000,
                 'coverImage' => 'https://i.imgur.com/blCuKOt.jpg',
                 'difficulty' => 5,
                 'evaluation' => 5,
                 'altitude' => 2000,
-                'article_id' => 3,
-                'classification_id' => 2,
+                'article_id' =>  autoIncrementTweak(3),
+                'classification_id' =>  autoIncrementTweak(2),
             ],
             [
                 'title' => '慶記步道',
-                'location_id' => 5,
+                'location_id' =>  autoIncrementTweak(5),
                 'distance' => 1000,
                 'coverImage' => 'https://i.imgur.com/lyUntWJ.jpg',
                 'difficulty' => 5,
                 'evaluation' => 5,
                 'altitude' => 1000,
-                'article_id' => 2,
-                'classification_id' => 1,
+                'article_id' =>  autoIncrementTweak(2),
+                'classification_id' =>  autoIncrementTweak(1),
             ],
 
         ];
