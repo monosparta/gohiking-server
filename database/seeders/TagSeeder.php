e90bea3d4755b869b59dc8fea6767c155ab7f7bc
ZongYing_Lin <kwe0349@gmail.com>
2021-03-26 19:45:04 +0800

    update Tag Modle/Seeder and add tags.json datalist

--- database/seeders/TagSeeder.php
+++ database/seeders/TagSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Storage as FacadesStorage;
 
 class TagSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class TagSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $json = FacadesStorage::disk('local')->get('tags.json');
+        $json = json_decode($json, true);
+        foreach($json as $data){
+            $tag=new Tag();
+            $tag->tagName=$data['tagName'];
+        }
     }
 }
