9fe309a7110a2364f819e6bc1b3cc6e729017c22
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2021-03-31 12:06:28 +0800

    Change gender to nullable

--- database/migrations/2021_03_30_080856_change_gender_to_nullable.php
+++ database/migrations/2021_03_30_080856_change_gender_to_nullable.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddDefaultValueToGenderOfUsers extends Migration
+class ChangeGenderToNullable extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AddDefaultValueToGenderOfUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('gender')->default(-1)->change();
+            $table->boolean('gender')->nullable()->change();
         });
     }
 
@@ -26,7 +26,7 @@ class AddDefaultValueToGenderOfUsers extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('gender')->default(null)->change();
+            $table->boolean('gender')->nullable(false)->change();
         });
     }
 }
