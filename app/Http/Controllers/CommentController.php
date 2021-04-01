08ed0217c3f3b9753ccb162aa213e22743ee2e90
蔡名彥 <a976543257@gmail.com>
2021-04-01 13:39:25 +0800

    upload s3

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -31,6 +31,7 @@ class CommentController extends Controller
      */
     public function store(Request $request)
     {
+        $s3_filePaths = [];
         // //驗證資料
         $validator = Validator::make($request->all(), $this->rule(), $this->errorMassage());
         if ($validator->fails()) {
@@ -41,40 +42,39 @@ class CommentController extends Controller
             return ['status' => 0, 'massage' => $error];
         }
         //新增評論
-        $comments=new Comment();
-        $comments->user_id=$request->user_id;
-        $comments->trail_id=$request->trail_id;
-        $comments->date=$request->date;
-        $comments->star=$request->star;
-        $comments->difficulty=$request->difficulty;
-        $comments->beauty=$request->beauty;
-        $comments->duration=$request->duration;
-        $comments->content=$request->content;
-        $comments->save();//新增
+        $comments = new Comment();
+        $comments->user_id = $request->user_id;
+        $comments->trail_id = $request->trail_id;
+        $comments->date = $request->date;
+        $comments->star = $request->star;
+        $comments->difficulty = $request->difficulty;
+        $comments->beauty = $request->beauty;
+        $comments->duration = $request->duration;
+        $comments->content = $request->content;
+        $comments->save(); //新增
         //取的最新一筆 該使用者新增的comment_id
-        $last_comments_id=Comment::select('id')->where('user_id','=',$request->user_id)->latest('id')->first();
+        $last_comments_id = Comment::select('id')->where('user_id', '=', $request->user_id)->latest('id')->first();
         //評論圖片如果有上傳，才執行
-        if(isset($request->images) && isset($request->tag_id)){
-            if($this->upload_s3($request->images)){
-                $s3_filePaths=$this->upload_s3($request->images);
-            }
-            else{
-                $s3_filePaths='';
-            }
-            $tags=explode(',',$request->tag_id);//分割傳來圖片的tag數字
-            for($i=0;$i<count($s3_filePaths);$i++)//看有幾筆，就新增幾筆
+        if (isset($request->images) && isset($request->tag_id)) {
+            $tags = explode(',', $request->tag_id); //分割傳來圖片的tag數字
+            for ($i = 0; $i < count($tags); $i++) //看有幾筆，就新增幾筆
             {
-                $commentsImages=new CommentsImage();
-                $commentsImages->comment_id=$last_comments_id->id;
-                $commentsImages->user_id=$request->user_id;
-                $commentsImages->s3_filePath=$s3_filePaths[$i];//放置S3URL
-                $commentsImages->tag_id=$tags[$i];
-                $commentsImages->save();
+                $a = '';
+                foreach ($request->file('images') as  $value) {
+                    $a .= $value->getClientOriginalName();
+                }
+                return $a;
+                // $path = $this->upload_s3($request->images[$i]);
+                // $commentsImages = new CommentsImage();
+                // $commentsImages->comment_id = $last_comments_id->id;
+                // $commentsImages->user_id = $request->user_id;
+                // $commentsImages->s3_filePath =  $path; //放置S3URL
+                // $commentsImages->tag_id = $tags[$i];
+                // $commentsImages->save();
             }
         }
 
-        return Comment::with('commentsImages.tag')->where('trail_id','=',$request->trail_id)->get();
-     Storage::disk('s3')->exists('1.jpg');
+        return Comment::with('commentsImages.tag')->where('trail_id', '=', $request->trail_id)->get();
     }
 
     /**
@@ -85,65 +85,63 @@ class CommentController extends Controller
      */
     public function show($id)
     {
-        $totalPeople=count(Comment::select('star') ->where('trail_id','=',$id)->get());
-        $avgStar=Comment::select('star') ->where('trail_id','=',$id)->avg('star');
-        $stars=Comment::select('star',DB::raw('count(*) as count'))
-        ->where('trail_id','=',$id)
-        ->groupBy('star')
-        ->get();
+        $totalPeople = count(Comment::select('star')->where('trail_id', '=', $id)->get());
+        $avgStar = Comment::select('star')->where('trail_id', '=', $id)->avg('star');
+        $stars = Comment::select('star', DB::raw('count(*) as count'))
+            ->where('trail_id', '=', $id)
+            ->groupBy('star')
+            ->get();
         //starts 取的資料是統計資料庫的 count數字1~5 不方便前端存取
         //startsgroup 回傳 前端方便存取的格式
-        $starsgroup=[
-            "one"=>"0",
-            "two"=>"0",
-            "three"=>"0",
-            "four"=>"0",
-            "five"=>"0"
+        $starsgroup = [
+            "one" => "0",
+            "two" => "0",
+            "three" => "0",
+            "four" => "0",
+            "five" => "0"
         ];
         //判斷 $stars的星級是哪一個，再塞入 starsgroup $stars人數
-        foreach($stars as $key=>$value)
-        {
-            switch ($value->star){
+        foreach ($stars as $key => $value) {
+            switch ($value->star) {
                 case 1:
-                    $starsgroup['one']=$value->count;
+                    $starsgroup['one'] = $value->count;
                     break;
                 case 2:
-                    $starsgroup['two']=$value->count;
+                    $starsgroup['two'] = $value->count;
                     break;
                 case 3:
-                    $starsgroup['three']=$value->count;
+                    $starsgroup['three'] = $value->count;
                     break;
                 case 4:
-                    $starsgroup['four']=$value->count;
+                    $starsgroup['four'] = $value->count;
                     break;
                 case 5:
-                    $starsgroup['five']=$value->count;
+                    $starsgroup['five'] = $value->count;
                     break;
             }
         }
         //取的目前的comment data
-        $comments=Comment::with('user:id,name','commentsImages:id,comment_id,s3_filePath,tag_id')
-        ->where('trail_id','=',$id)
-        ->withCount(['userLikeComment as like'=>function(Builder $likequery){
-            $likequery->where('status',1);//1 = like count 算出status等於1的有多少
-        },'userLikeComment as dislike'=>function(Builder $dislikequery){
-            $dislikequery->where('status',-1);//-1 = dislike count 算出status等於-1的有多少
-        }
-        ])->get();
-        foreach($comments as $key=>$values)
-        {
-            foreach($comments[$key]['commentsImages'] as $value)
-            {
+        $comments = Comment::with('user:id,name', 'commentsImages:id,comment_id,s3_filePath,tag_id')
+            ->where('trail_id', '=', $id)
+            ->withCount([
+                'userLikeComment as like' => function (Builder $likequery) {
+                    $likequery->where('status', 1); //1 = like count 算出status等於1的有多少
+                }, 'userLikeComment as dislike' => function (Builder $dislikequery) {
+                    $dislikequery->where('status', -1); //-1 = dislike count 算出status等於-1的有多少
+                }
+            ])->get();
+        foreach ($comments as $key => $values) {
+            foreach ($comments[$key]['commentsImages'] as $value) {
                 //取得圖片URL
-                $value['s3_url']=$this->getFileUrl_s3($value->s3_filePath);
+                $value['s3_url'] = $this->getFileUrl_s3($value->s3_filePath);
             }
         }
 
         return response()->json(array(
-            'totalPeople'=>$totalPeople,//評論總人數
-            'avgStar'=>$avgStar,//平均星數
-            'stars'=>$starsgroup,//各星級人數
-            'comments'=>$comments,//評論內容
+            'totalPeople' => $totalPeople, //評論總人數
+            'avgStar' => $avgStar, //平均星數
+            'stars' => $starsgroup, //各星級人數
+            'comments' => $comments, //評論內容
         ));
     }
 
@@ -156,7 +154,6 @@ class CommentController extends Controller
      */
     public function update(Request $request, $id)
     {
-        
     }
 
     /**
@@ -174,14 +171,14 @@ class CommentController extends Controller
     {
         return
             [
-                'user_id'=>'bail|required',
-                'trail_id'=>'bail|required',
-                'date'=>'bail|required|date:' . date("Y-m-d"),
-                'star'=>'bail|required',
-                'difficulty'=>'bail|required',
-                'beauty'=>'bail|required',
-                'duration'=>'bail|required',
-                'content'=>'bail|required',
+                'user_id' => 'bail|required',
+                'trail_id' => 'bail|required',
+                'date' => 'bail|required|date:' . date("Y-m-d"),
+                'star' => 'bail|required',
+                'difficulty' => 'bail|required',
+                'beauty' => 'bail|required',
+                'duration' => 'bail|required',
+                'content' => 'bail|required',
             ];
     }
 
@@ -204,11 +201,15 @@ class CommentController extends Controller
     {
         $date = new DateTime();
         $timestamp =  $date->getTimestamp();
-        list($baseType, $image) = explode(';', $uploadImage);
-        list(, $image) = explode(',', $image);
-        $image = base64_decode($image);
         $filePath = 'imgs/' . $timestamp . '.jpg';
-        return Storage::disk('s3')->put($filePath, $image) ? $filePath : false;
+        if (gettype($uploadImage) == 'object') {
+            return Storage::disk('s3')->putFileAs('imgs1', $uploadImage, $timestamp . '.jpg') ? $filePath : false;
+        } else {
+            list($baseType, $image) = explode(';', $uploadImage);
+            list(, $image) = explode(',', $image);
+            $image = base64_decode($image);
+            return Storage::disk('s3')->put('imgs/' . $timestamp . '.jpg', $image) ? $filePath : false;
+        }
     }
 
     private function getFileUrl_s3($fileName)
