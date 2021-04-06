52d892b0069c3d7033ab7b780c735261cf32eccb
蔡名彥 <a976543257@gmail.com>
2021-04-06 22:01:16 +0800

    update cors

--- routes/api.php
+++ routes/api.php
@@ -28,41 +28,42 @@ use App\Http\Controllers\FavoritesController;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::middleware('cors')->group(function () {
+    Route::get('incorrectToken', function () {
+        return response(['Status' => 'incorrect token!'], 401);
+    })->name('incorrectToken');
 
-Route::get('incorrectToken', function () {
-    return response(['Status' => 'incorrect token!'], 401);
-})->name('incorrectToken');
+    Route::post('register', [PassportAuthController::class, 'register']);
+    Route::middleware('auth:api')->post('profile', [PassportAuthController::class, 'createProfile']);
 
-Route::post('register', [PassportAuthController::class, 'register']);
-Route::middleware('auth:api')->post('profile', [PassportAuthController::class, 'createProfile']);
+    Route::post('login', [PassportAuthController::class, 'login']);
 
-Route::post('login', [PassportAuthController::class, 'login']);
+    Route::post('auth/social/callback', [SocialController::class, 'handleSocialCallback']);
 
-Route::post('auth/social/callback', [SocialController::class, 'handleSocialCallback']);
+    Route::post('/password/forget', [PassportAuthController::class, 'forgetPassword']);
+    Route::post('/password/confirm', [PassportAuthController::class, 'confirmVerificationCodes']);
+    Route::middleware('auth:api')->post('/password/change', [PassportAuthController::class, 'changePassword']);
 
-Route::post('/password/forget', [PassportAuthController::class, 'forgetPassword']);
-Route::post('/password/confirm', [PassportAuthController::class, 'confirmVerificationCodes']);
-Route::middleware('auth:api')->post('/password/change', [PassportAuthController::class, 'changePassword']);
+    Route::middleware('auth:api')->get('index', function () {
+        return ['Status' => 'Logged!'];
+    });
 
-Route::middleware('auth:api')->get('index', function () {
-    return ['Status' => 'Logged!'];
+    Route::resource('/collection', CollectionController::class);
+
+    Route::resource('/trail', TrailController::class);
+
+    Route::resource('/user', UserController::class);
+
+    Route::resource('/home', HomeController::class);
+
+    Route::resource('/article', ArticleController::class);
+
+    Route::resource('/classification', ClassificationController::class);
+
+    Route::resource('/favorite', FavoritesController::class);
+    Route::get('/favorites', [FavoritesController::class, 'Inquire']);
+    Route::post('/favorite/delete', [FavoriteController::class, 'delete']);
+
+    Route::resource('/comment', CommentController::class);
+    Route::resource('/likeComment', UserLikeCommentController::class);
 });
-
-Route::resource('/collection', CollectionController::class);
-
-Route::resource('/trail', TrailController::class);
-
-Route::resource('/user', UserController::class);
-
-Route::resource('/home', HomeController::class);
-
-Route::resource('/article', ArticleController::class);
-
-Route::resource('/classification', ClassificationController::class);
-
-Route::resource('/favorite', FavoritesController::class);
-Route::get('/favorites', [FavoritesController::class, 'Inquire']);
-Route::post('/favorite/delete', [FavoriteController::class, 'delete']);
-
-Route::resource('/comment',CommentController::class);
-Route::resource('/likeComment',UserLikeCommentController::class);
