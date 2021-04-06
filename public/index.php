8ca5899f21d63bec70eb4acc37c2a6c350447e27
蔡名彥 <a976543257@gmail.com>
2021-04-07 00:06:19 +0800

    update cors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-header('Access-Control-Allow-Origin:');
+header('Access-Control-Allow-Origin:*');
 header('Access-Control-Allow-Methods:POST, GET');
 header('Access-Control-Allow-Headers:*');
 
