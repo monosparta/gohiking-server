5f6dde48d8eebfeb586adffe0f02da3654426361
蔡名彥 <a976543257@gmail.com>
2021-04-07 13:24:24 +0800

    cors Access-Control-Allow-Methods *

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin:*');
-header('Access-Control-Allow-Methods:POST, GET');
+header('Access-Control-Allow-Methods:*');
 header('Access-Control-Allow-Headers:*');
 
 use Illuminate\Contracts\Http\Kernel;
